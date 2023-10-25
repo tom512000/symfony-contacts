705518954187f424b358c9cfbf227793fb15f1c8
siko0001 <tom.sikora@etudiant.univ-reims.fr>
2023-10-25 18:14:37 +0200

    15.4. Détails d'un contact, version naïve

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -17,5 +18,19 @@ class ContactController extends AbstractController
         return $this->render('contact/index.html.twig', [
             'contacts' => $contacts,
             'pageTitle' => 'Liste des contacts']);
+    }
+
+    #[Route('/contact/{contactId}', name: 'app_contact_show', requirements: ['contactId' => '\d+'])]
+    public function show(int $contactId, ContactRepository $rep): Response
+    {
+        $contact = $rep->find($contactId);
+
+        if (!$contact) {
+            throw new NotFoundHttpException('Contact introuvable');
+        }
+
+        return $this->render('contact/show.html.twig', [
+            'contact' => $contact,
+        ]);
     }
 }
